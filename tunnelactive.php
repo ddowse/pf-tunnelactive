f6a9bedb30b4825a9c9471b54255bd2a66cc9eac
Daniel Dowse <support@mylinuxadmin.de>
2020-11-20 17:23:16 +0100

    Changed echo line for check

--- tunnelactive.php
+++ tunnelactive.php
@@ -1,8 +1,8 @@
 <?php
-/**
+/** 
  * @author    Daniel Dowse <support@mylinuxadmin.de>
  * @copyright 2020 Daniel Dowse
- * @version   0.1
+ * @version   0.1-alpha
  * @link      https://github.com/ddowse/pf-tunnelactive
  * THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
@@ -24,76 +24,78 @@ function Send_msg($notice)
     );
 } 
 
-function Check($clients) {
+function Check($clients)
+{
     foreach($clients as $check) {
-	    $status = $check['status'];
-           if (strcmp($status, "up") != 0) { 
-	    return "true"; } 
+        $status = $check['status'];
+        if (strcmp($status, "up") != 0) { 
+            return "true"; 
+        } 
     }
-}	
+}    
 
 while(true) {
         
     echo "Sleeping (" . $argv[2] . ")..." ;
             
-        $clients = openvpn_get_active_clients();
+    $clients = openvpn_get_active_clients();
         
-        if (Check($clients)) {  
+    if (Check($clients)) {  
                     
-               echo "\nReconnecting...\n";    
+           echo "\nReconnecting...\n";    
         
-            foreach ($clients as $client) {        
-                   $extras['vpnmode'] = "client";
-                   $extras['id'] = $client['vpnid'];
+        foreach ($clients as $client) {        
+               $extras['vpnmode'] = "client";
+               $extras['id'] = $client['vpnid'];
         
-                   service_control_stop("openvpn", $extras);
-                   echo "Stopping: " .  $client['name'];
+               service_control_stop("openvpn", $extras);
+               echo "Stopping: " .  $client['name'];
         
-                for ($i=0;$i<3;$i++) {
-                    sleep(1);
-                    echo " * ";
-                }
-                           echo "\n";
+            for ($i=0;$i<3;$i++) {
+                sleep(1);
+                echo " * ";
             }
+                       echo "\n";
+        }
         
-            $index=count($clients);
+        $index=count($clients);
         
-            foreach (array_reverse($clients) as $client) {
-                $index--;
-                echo "Starting:" . $client['name'];
-                $extras['vpnmode'] = "client";
-                $extras['id'] = $client['vpnid'];
+        foreach (array_reverse($clients) as $client) {
+            $index--;
+            echo "Starting:" . $client['name'];
+            $extras['vpnmode'] = "client";
+            $extras['id'] = $client['vpnid'];
             
-                service_control_start("openvpn", $extras);
+            service_control_start("openvpn", $extras);
         
-                for ($i=0;$i<$argv[1];$i++) {
-                       sleep(1);
-                       echo " * ";
-                       // Reread the Status!
-                       $update = openvpn_get_active_clients();
-                       $status = $update[$index]['status'];
-        
-                    if (strcmp($status, "up") == 0) { 
-                        echo "OK\n";
-                        $i = $argv[1];
-                    }  
-                } 
-        
-                if (strcmp($status, "up") == 0) { 
-                } else {
-                    reset($clients);
-                    echo "failed.\n";
-                    break;
-                }    
-            }
-                Send_msg("OpenVPN Tunnels restarted");    
-             
-        } else {
-            for ($i=0;$i<$argv[2];$i++) {
+            for ($i=0;$i<$argv[1];$i++) {
                    sleep(1);
                    echo " * ";
+                   // Reread the Status!
+                   $update = openvpn_get_active_clients();
+                   $status = $update[$index]['status'];
+        
+                if (strcmp($status, "up") == 0) { 
+                    echo "OK\n";
+                    $i = $argv[1];
+                }  
             } 
-            echo "\n";
+        
+            if (strcmp($status, "up") == 0) { 
+            } else {
+                reset($clients);
+                echo "failed.\n";
+                break;
+            }    
         }
+            Send_msg("OpenVPN Tunnels restarted");    
+             
+    } else {
+        for ($i=0;$i<$argv[2];$i++) {
+               sleep(1);
+               echo " * ";
+        } 
+        echo "Checking\n";
     }
+}
 
